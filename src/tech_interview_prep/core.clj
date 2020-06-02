6b707a619ba80419e71009d99ebb8b9fe37bdeea
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-02 16:38:51 -0400

    Max profit brute force solution

--- src/tech_interview_prep/core.clj
+++ src/tech_interview_prep/core.clj
@@ -91,3 +91,32 @@
 ;; => true
 (contains-duplicate-c [1 1 1 3 3 4 3 2 4 1])
 ;; => true
+
+
+;; Say you have an array for which the ith element is the price of a given stock on day i.
+
+;; If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+;; Note that you cannot sell a stock before you buy one.
+
+(defn max-profit-a [prices]
+  (loop [i 0
+         j 1
+         max-profit 0]
+    (if (= i (dec (count prices)))
+      max-profit
+      (if (= j (count prices))
+        (recur (inc i) (+ i 2) max-profit)
+        (let [profit (- (prices j) (prices i))]
+          (if (> profit max-profit)
+            (do
+              (println "i" i "j" j) (recur i (inc j) profit))
+            (do
+              (println "i" i "j" j) (recur i (inc j) max-profit))))))))
+;; => #'tech-interview-prep.core/max-profit
+
+(max-profit-a [7 1 5 3 6 4])
+;; => 5
+
+(max-profit-a [7 6 4 3 1])
+;; => 0
