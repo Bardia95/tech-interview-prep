0fd6d4ef9a109a30de80f8d1e00b841a43d22574
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-10 00:08:35 -0400

    add test for reversing linked list

--- src/tech_interview_prep/week_2.clj
+++ src/tech_interview_prep/week_2.clj
@@ -4,7 +4,21 @@
 (defrecord ListNode [val next])
 ;; => tech_interview_prep.week_2.ListNode
 
-(defn reverse-list [head]
+(defn reverse-list
+  {:doc "Reverse a linked list - iterative solution"
+   :test #(let [D (->ListNode 4 nil)
+                C (->ListNode 3 D)
+                B (->ListNode 2 C)
+                A (->ListNode 1 B)
+                H (->ListNode 1 nil)
+                G (->ListNode 2 H)
+                F (->ListNode 3 G)
+                E (->ListNode 4 F)
+                head A
+                reversed-head E]
+            (do
+              (assert (= (reverse-list head) reversed-head))))}
+  [head]
   (loop [prev nil
          curr head]
     (if (= curr nil)
@@ -16,6 +30,9 @@
         (recur prev curr)))))
 ;; => #'tech-interview-prep.week-2/reverse-list
 
+
+(test #'reverse-list)
+;; => :ok
 
 
 (def D (->ListNode 4 nil))
