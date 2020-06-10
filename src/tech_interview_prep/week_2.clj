9fbc7c9aacc58b2f87d741544f9f6af6cac040bf
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-10 00:02:01 -0400

    fix reverse list examples

--- src/tech_interview_prep/week_2.clj
+++ src/tech_interview_prep/week_2.clj
@@ -14,25 +14,28 @@
             prev curr
             curr next-temp]
         (recur prev curr)))))
-
 ;; => #'tech-interview-prep.week-2/reverse-list
 
 
 
-(def D (->ListNode 0 nil))
+(def D (->ListNode 4 nil))
 ;; => #'tech-interview-prep.week-2/D
 
-(def C (->ListNode 0 D))
+(def C (->ListNode 3 D))
 ;; => #'tech-interview-prep.week-2/C
 
-(def B (->ListNode 0 C))
+(def B (->ListNode 2 C))
 ;; => #'tech-interview-prep.week-2/B
 
-(def A (->ListNode 0 B))
+(def A (->ListNode 1 B))
 ;; => #'tech-interview-prep.week-2/A
 
-(def head A);; => #'tech-interview-prep.week-2/head
+(def head A)
+;; => #'tech-interview-prep.week-2/head
 
+head
+;; => #tech_interview_prep.week_2.ListNode{:val 1, :next #tech_interview_prep.week_2.ListNode{:val 2, :next #tech_interview_prep.week_2.ListNode{:val 3, :next #tech_interview_prep.week_2.ListNode{:val 4, :next nil}}}}
 
 (reverse-list head)
-;; => #tech_interview_prep.week_2.ListNode{:val 0, :next #tech_interview_prep.week_2.ListNode{:val 0, :next #tech_interview_prep.week_2.ListNode{:val 0, :next #tech_interview_prep.week_2.ListNode{:val 0, :next nil}}}}
+;; => #tech_interview_prep.week_2.ListNode{:val 4, :next #tech_interview_prep.week_2.ListNode{:val 3, :next #tech_interview_prep.week_2.ListNode{:val 2, :next #tech_interview_prep.week_2.ListNode{:val 1, :next nil}}}}
+
