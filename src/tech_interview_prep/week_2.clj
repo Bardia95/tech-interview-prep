7dd49848bacc3b3539e0a0ea119b139572f27ec0
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-10 20:48:14 -0400

    max area solution

--- src/tech_interview_prep/week_2.clj
+++ src/tech_interview_prep/week_2.clj
@@ -94,3 +94,31 @@
           (recur (:next slow)
                  (:next (:next fast))))))))
 ;; => #'tech-interview-prep.week-2/has-cycle-b?
+
+
+(defn max-area
+  {:doc "Given `n` non-negative integers `a1`, `a2`, ..., `an` ,
+         where each represents a point at coordinate (`i`, `ai`).
+
+         `n` vertical lines are drawn such that the two endpoints
+         of line `i` is at (`i`, `ai`) and (`i`, `0`).
+
+         Find two lines, which together with x-axis forms a container,
+         such that the container contains the most water."
+   :test #(do
+            (assert (= (max-area [1 8 6 2 5 4 8 3 7]) 49)))}
+  [height]
+  (loop [maxarea 0
+         l 0
+         r (dec (count height))]
+    (if (= l r)
+      maxarea
+      (let [maxarea (max maxarea (* (min (height l) (height r)) (Math/abs (- r l))))]
+        (if (< (height l) (height r))
+          (recur maxarea (inc l) r)
+          (recur maxarea l (dec r)))))))
+;; => #'tech-interview-prep.week-2/max-area
+
+
+(test #'max-area)
+;; => :ok
